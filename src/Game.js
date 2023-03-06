6f5734deaaba57d160a76b467865e33d06f4a73f
Amanda Stern <amanda.stern.73@gmail.com>
2023-03-06 16:19:08 -0500

    toggle Die to lock individual die

--- src/Game.js
+++ src/Game.js
@@ -31,6 +31,7 @@ class Game extends Component {
     };
     this.roll = this.roll.bind(this);
     this.doScore = this.doScore.bind(this);
+    this.toggleLocked = this.toggleLocked.bind(this)
   }
 
   roll(evt) {
@@ -46,11 +47,11 @@ class Game extends Component {
 
   toggleLocked(idx) {
     // toggle whether idx is in locked or not
-    this.setState(st => ({
+    this.setState((st) => ({
       locked: [
         ...st.locked.slice(0, idx),
         !st.locked[idx],
-        ...st.locked.slice(idx + 1)
+        ...st.locked.slice(idx + 1),
       ]
     }));
   }
