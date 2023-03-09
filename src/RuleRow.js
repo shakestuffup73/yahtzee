7c4b83ade8641ca06ceab1de7e6be66b68c2b086
Amanda Stern <amanda.stern.73@gmail.com>
2023-03-09 15:26:07 -0500

    describe point distribution for all hands in rules

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -3,15 +3,20 @@ import './RuleRow.css'
 
 class RuleRow extends Component {
   render() {
+    
+    const {score, name, doScore} = this.props
+    const disabled = score != undefined
+
     return (
-      <tr className={`RuleRow RuleRow-${this.props.score === undefined 
-      ? "active" : "disabled"}`} 
-      onClick={this.props.doScore === undefined 
-      ? this.props.doScore : null }>
-        <td className="RuleRow-name">{this.props.name}</td>
-        <td className="RuleRow-score">{this.props.score}</td>
+      <tr className={`RuleRow RuleRow-${
+        disabled ? "disabled" : "active"
+        }`} 
+        onClick={disabled ? null : doScore }
+      >
+        <td className="RuleRow-name">{name}</td>
+        <td className="RuleRow-score">{score}</td>
       </tr>
-    )
+    ) 
   }
 }
 
